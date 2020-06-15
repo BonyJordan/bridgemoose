3b817e597405b99ac76d5c5327d9fdde6f81c32b
Jordan Lampe <jordan@jyjy.org>
2020-06-15 00:03:20 -0400

    Add analyze_many_deals

--- dds/Python.cpp
+++ dds/Python.cpp
@@ -252,6 +252,94 @@ dds_solve_deal(PyObject* self, PyObject* args)
     return Py_BuildValue("i", 13-futs.score[0]);
 }
 
+// Returns Py_None if okay, else an error of some sort to be passed along
+static PyObject*
+load_boards_pbn(PyObject* py_deal_list, struct boardsPBN& boards,
+    int count_by_player[MAXNOOFBOARDS][4], const int ctSuit[3],
+    const int ctRank[3], int strain_id, int first_dir_id)
+{
+    int num_deals = 0;
+
+    PyObject* py_iter = PyObject_GetIter(py_deal_list);
+    if (py_iter == NULL)
+        return NULL;
+
+    PyObject* py_deal;
+    while ((py_deal = PyIter_Next(py_iter)))
+    {
+        if (num_deals >= MAXNOOFBOARDS) {
+            Py_DECREF(py_deal);
+            Py_DECREF(py_iter);
+            return PyErr_Format(PyExc_IndexError,
+                "At most %d deals at once", MAXNOOFBOARDS);
+        }
+
+        const char* h[4];
+
+        if (!PyArg_ParseTuple(py_deal, "ssss", &h[0], &h[1], &h[2], &h[3]))
+        {
+            Py_DECREF(py_deal);
+            Py_DECREF(py_iter);
+            return NULL;
+        }
+
+        boards.target[num_deals] = -1;
+        boards.solutions[num_deals] = 3;
+        boards.mode[num_deals] = 0;
+
+        boards.deals[num_deals].trump = strain_id;
+        boards.deals[num_deals].first = first_dir_id; 
+        for (int i=0 ; i<3 ; i++) {
+            boards.deals[num_deals].currentTrickSuit[i] = ctSuit[i];
+            boards.deals[num_deals].currentTrickRank[i] = ctRank[i];
+        }
+
+        int i = 0;
+        boards.deals[num_deals].remainCards[i++] = 'N';
+        boards.deals[num_deals].remainCards[i++] = ':';
+
+        for (int j=0 ; j<4 ; j++) {
+            int count = 0;
+	    if (j != 0) {
+		if (i >= 78) {
+                    Py_DECREF(py_deal);
+                    Py_DECREF(py_iter);
+                    return PyErr_Format(PyExc_ValueError,
+                        "Too many cards specified");
+                }
+		boards.deals[num_deals].remainCards[i++] = ' ';
+	    }
+
+            count_by_player[num_deals][j] = 0;
+            for (const char* cp = h[j] ; *cp ; cp++) {
+                if (i >= 78) {
+                    Py_DECREF(py_deal);
+                    return PyErr_Format(PyExc_ValueError,
+                        "Too many cards specified");
+                }
+
+                if (*cp == '-')
+                    continue;
+                else if (*cp == '/')
+                    boards.deals[num_deals].remainCards[i++] = '.';
+                else {
+                    count_by_player[num_deals][j] += 1;
+                    boards.deals[num_deals].remainCards[i++] = *cp;
+                }
+            }
+        }
+	boards.deals[num_deals].remainCards[i] = 0;
+
+        num_deals += 1;
+        Py_DECREF(py_deal);
+    }
+    boards.noOfBoards = num_deals;
+
+    Py_DECREF(py_iter);
+    return Py_None;
+}
+
+
 static PyObject*
 dds_solve_many_plays(PyObject* self, PyObject* args)
 {
@@ -293,83 +381,14 @@ dds_solve_many_plays(PyObject* self, PyObject* args)
     if (strain_id == -1)
         return PyErr_Format(PyExc_ValueError, "Bad strain '%s'", strain);
 
-    PyObject* iter = PyObject_GetIter(py_list);
-    if (iter == NULL)
-        return NULL;
-
     struct boardsPBN boards;
-    int num_deals = 0;
     int count_by_player[MAXNOOFBOARDS][4];
-
-    PyObject* py_deal;
-    while ((py_deal = PyIter_Next(iter)))
-    {
-        if (num_deals >= MAXNOOFBOARDS) {
-            Py_DECREF(py_deal);
-            return PyErr_Format(PyExc_IndexError,
-                "At most %d deals at once", MAXNOOFBOARDS);
-        }
-
-        const char* h[4];
-
-        if (!PyArg_ParseTuple(py_deal, "ssss", &h[0], &h[1], &h[2], &h[3]))
-        {
-            Py_DECREF(py_deal);
-            return NULL;
-        }
-
-        boards.target[num_deals] = -1;
-        boards.solutions[num_deals] = 3;
-        boards.mode[num_deals] = 0;
-
-        boards.deals[num_deals].trump = strain_id;
-        boards.deals[num_deals].first = (play_dir_id + (4 - ctNonZero)) % 4;
-        for (int i=0 ; i<3 ; i++) {
-            boards.deals[num_deals].currentTrickSuit[i] = ctSuit[i];
-            boards.deals[num_deals].currentTrickRank[i] = ctRank[i];
-        }
-
-        int i = 0;
-        boards.deals[num_deals].remainCards[i++] = 'N';
-        boards.deals[num_deals].remainCards[i++] = ':';
-
-        for (int j=0 ; j<4 ; j++) {
-            int count = 0;
-	    if (j != 0) {
-		if (i >= 78) {
-                    Py_DECREF(py_deal);
-                    return PyErr_Format(PyExc_ValueError,
-                        "Too many cards specified");
-                }
-		boards.deals[num_deals].remainCards[i++] = ' ';
-	    }
-
-            count_by_player[num_deals][j] = 0;
-            for (const char* cp = h[j] ; *cp ; cp++) {
-                if (i >= 78) {
-                    Py_DECREF(py_deal);
-                    return PyErr_Format(PyExc_ValueError,
-                        "Too many cards specified");
-                }
-
-                if (*cp == '-')
-                    continue;
-                else if (*cp == '/')
-                    boards.deals[num_deals].remainCards[i++] = '.';
-                else {
-                    count_by_player[num_deals][j] += 1;
-                    boards.deals[num_deals].remainCards[i++] = *cp;
-                }
-            }
-        }
-	boards.deals[num_deals].remainCards[i] = 0;
-
-        num_deals += 1;
-        Py_DECREF(py_deal);
-    }
+    PyObject* r = load_boards_pbn(py_list, boards, count_by_player, ctSuit,
+        ctRank, strain_id, (play_dir_id + (4 - ctNonZero)) % 4);
+    if (r != Py_None)
+        return r;
 
     struct solvedBoards sb;
-    boards.noOfBoards = num_deals;
     int ret = SolveAllBoards(&boards, &sb);
     if (ret < 0)
         return dds_error(ret);
@@ -429,29 +448,133 @@ dds_solve_many_plays(PyObject* self, PyObject* args)
     return out_list;
 }
 
+static PyObject*
+dds_analyze_many_plays(PyObject* self, PyObject* args)
+{
+    PyObject* py_list;
+    const char* declarer_dir;
+    const char* strain;
+    const char* history;
+    if (!PyArg_ParseTuple(args, "Osss", &py_list, &declarer_dir, &strain,
+        &history))
+    //
+        return NULL;
 
-static PyMethodDef DdsMethods[] = {
-    {"solve_deal", dds_solve_deal, METH_VARARGS,
+    int historyLen = strlen(history);
+    if (historyLen > 104)
+        return PyErr_Format(PyExc_ValueError, "Play history too long");
+    if (historyLen % 2 != 0)
+        return PyErr_Format(PyExc_ValueError, "Play history odd length");
+
+    int ctSuit[3];
+    int ctRank[3];
+    for (int i=0 ; i<3 ; i++) {
+        ctSuit[i] = 0;
+        ctRank[i] = 0;
+    }
+
+    int declarer_dir_id = string_to_dir(declarer_dir);
+    if (declarer_dir_id == -1)
+        return PyErr_Format(PyExc_ValueError, "Bad declarer '%s'", declarer_dir);
+
+    int strain_id = string_to_strain(strain);
+    if (strain_id == -1)
+        return PyErr_Format(PyExc_ValueError, "Bad strain '%s'", strain);
+
+    struct boardsPBN boards;
+    int count_by_player[MAXNOOFBOARDS][4];
+
+    PyObject* r = load_boards_pbn(py_list, boards,
+        count_by_player, ctSuit, ctRank, strain_id, (declarer_dir_id+1)%4);
+    if (r != Py_None)
+        return r;
+
+    struct playTracesPBN traces;
+    traces.noOfBoards = boards.noOfBoards;
+    for (int i=0 ; i<boards.noOfBoards ; i++) {
+        traces.plays[i].number = historyLen / 2;
+        strcpy(traces.plays[i].cards, history);
+    }
+
+    struct solvedPlays solved;
+
+    int ret = AnalyseAllPlaysPBN(&boards, &traces, &solved, 1);
+    if (ret < 0)
+        return dds_error(ret);
+
+    /// Construct the result
+    PyObject* out_list = PyList_New(solved.noOfBoards);
+    if (out_list == NULL)
+        return NULL;
+        
+    for (int i=0 ; i<solved.noOfBoards ; i++) {
+        PyObject* board_list = PyList_New(solved.solved[i].number);
+        if (board_list == NULL) {
+            Py_DECREF(out_list);
+            return NULL;
+        }
+        for (int j=0 ; j<solved.solved[i].number ; j++) {
+            PyObject* py_long = PyLong_FromLong(solved.solved[i].tricks[j]);
+            if (py_long == NULL) {
+                Py_DECREF(board_list);
+                Py_DECREF(out_list);
+                return NULL;
+            }
+            PyList_SET_ITEM(board_list, j, py_long);
+        }
+        PyList_SET_ITEM(out_list, i, board_list);
+    }
+    return out_list;
+}
+
+
+const char* solve_deal_desc =
 "Solve a single deal\n"
 "Takes three parameters:\n"
 "   1. A bridgemoose.Deal object\n"
 "   2. Declarer (string 'W','N','E', or 'S')\n"
-"   3. Strain (string 'C','D','H',S', or 'N')"},
-    {"solve_many_deals", dds_solve_many_deals, METH_VARARGS,
+"   3. Strain (string 'C','D','H','S', or 'N')\n"
+"Returns an integer number of tricks\n";
+
+const char* solve_many_deals_desc =
 "Solve many deals\n"
 "Takes one parameter, an iterable of 3-tuples.  Each tuple is:\n"
 "   1. A bridgemoose.Deal object\n"
 "   2. Declarer (string 'W','N','E', or 'S')\n"
-"   3. Strain (string 'C','D','H',S', or 'N')"},
-    {"solve_many_plays", dds_solve_many_plays, METH_VARARGS,
+"   3. Strain (string 'C','D','H','S', or 'N')\n"
+"Returns a list of integer number of tricks\n";
+
+const char* solve_many_plays_desc =
 "Solve many plays\n"
 "Takes four parameters:\n"
-"   1. A list of 4-tuples, where each tuple is a (partial) hand in string\n"
-"      format,\n"
-"       starting with West.\n"
+"   1. A list of 4-tuples, where each item is a (partial) hand in string\n"
+"      format, starting with West.\n"
 "   2. The direction of the player on play ('W','N','E', or 'S')\n"
 "   3. Strain ('C','D','H','S', or 'N')\n"
-"   4. Trick so far; a string like 'C5CT' of up to 3 cards\n"},
+"   4. Trick so far; a string like 'C5CT' of up to 3 cards\n"
+"Output is a list (one per deal) of a list (one per card) of 2-tuples\n"
+"   in (card, tricks) format, such as ('C5', 3)\n"
+"   tricks are counted from the PoV of the player on play\n";
+
+const char* analyze_many_plays_desc =
+"Analyze many plays\n"
+"Takes four parameters:\n"
+"   1. A list of 4-tuples, where each item is hand in string\n"
+"      format, starting with West.\n"
+"   2. The direction of the declarer\n"
+"   3. Strain ('C',etc)\n"
+"   4. The common play history as a compressed string format, example:\n"
+"      'S4SJSQSAC4D2CKC5'\n"
+"\n"
+"Output is a list (one per deal) or a list (one per card) of integers\n"
+"   representing DD tricks (declarer's PoV).  This list is one longer\n"
+"   than the number of cards in parameter 4";
+
+static PyMethodDef DdsMethods[] = {
+    {"solve_deal", dds_solve_deal, METH_VARARGS, solve_deal_desc},
+    {"solve_many_deals", dds_solve_many_deals, METH_VARARGS, solve_many_deals_desc},
+    {"solve_many_plays", dds_solve_many_plays, METH_VARARGS, solve_many_plays_desc},
+    {"analyze_many_plays", dds_analyze_many_plays, METH_VARARGS, analyze_many_plays_desc},
     {NULL, NULL, 0, NULL}
 };
 
