3ed04c330a259a8d07e23b35ff9221da6dba1052
Jordan Lampe <jordan@jyjy.org>
2020-07-23 19:40:13 -0400

    change random_deals api

--- src/bridgemoose/random.py
+++ src/bridgemoose/random.py
@@ -1,6 +1,7 @@
 import random
 from .deal import Card, Deal, Hand
 from .direction import Direction
+from .play import PartialHand
 
 def parse_card_set(s):
     out = set()
@@ -17,63 +18,90 @@ def parse_card_set(s):
         raise ValueError("More than 13 cards: '%s'" % (s))
     return out
 
-def one_random_deal(fixed_cards={}, rng=None):
-    if rng is None:
-        rng = random
-        sorting = False
-    else:
-        sorting = True
-
-    cardset = set(Card.all())
-    known_cards = {d: set() for d in Direction.ALL}
-    for key, val in fixed_cards.items():
-        if isinstance(key, Direction):
-            key = str(key)
-        if not key in "NSEW":
-            raise ValueError("Bad key for fixed_cards")
-            
-        if isinstance(val, Hand):
-            known_cards[key] = val.cards
+class RestrictedDealer:
+    def __init__(self, west=None, north=None, east=None, south=None, rng=None):
+        if rng is None:
+            self.rng = random
+            self.sorting = False
         else:
-            known_cards[key] = parse_card_set(val)
+            self.sorting = True
 
-        cardset -= known_cards[key]
+        self.cardset = set(Card.all())
+        self.acceptors = dict()
+        self.known_cards = {d: set() for d in Direction.ALL}
 
-    # Must sort when
-    cardlist = list(cardset)
-    if sorting:
-        cardlist.sort()
-    rng.shuffle(cardlist)
-    n = 0
+        for d, spec in zip("WNES", [west, north, east, south]):
+            if spec is None:
+                continue
+            if isinstance(spec, str):
+                hand = Hand(spec)
+                self._process_card_set(d, hand.cards)
+            elif isinstance(spec, (Hand, PartialHand)):
+                self._process_card_set(d, spec.cards)
+            elif callable(spec):
+                self.acceptors[d] = spec
+            else:
+                self._process_card_set(d, set(spec))
 
-    hands = {}
-    for key in Direction.ALL:
-        known = known_cards[key]
-        k = 13 - len(known)
-        unknown = set(cardlist[n:n+k])
-        hands[key] = Hand(known | unknown)
-        n += k
-    assert n == len(cardlist), (n, len(cardlist))
-    return Deal(hands['W'],hands['N'],hands['E'],hands['S'])
+    def one_try(self):
+        # Must sort when
+        cardlist = list(self.cardset)
+        if self.sorting:
+            self.cardlist.sort()
+        self.rng.shuffle(cardlist)
+        n = 0
 
-def random_deals(count, accept=None, fixed_cards={}, rng=None, fail_count=100000):
-    """
+        hands = {}
+        for key in Direction.ALL:
+            known = self.known_cards[key]
+            k = 13 - len(known)
+            unknown = set(cardlist[n:n+k])
+            hands[key] = Hand(known | unknown)
+            n += k
+
+        assert n == len(cardlist), (n, len(cardlist))
+
+        for d, acc in self.acceptors.items():
+            if not acc(hands[d]):
+                return None
+        return Deal(hands['W'],hands['N'],hands['E'],hands['S'])
+
+    def _process_card_set(self, d, cset):
+        if len(cset) != 13:
+            raise ValueError("Require exactly 13 cards for %s" % (d,))
+        for x in cset:
+            if not isinstance(x, Card):
+                raise TypeError("Not a Card: %s" % (x))
+            if not x in self.cardset:
+                raise ValueError("Card used twice: %s" % (x))
+            self.cardset.remove(x)
+        self.known_cards[d] = cset
+
+def random_deals(count, west=None, north=None, east=None, south=None,
+    rng=None, fail_count=100000):
+    """\
 Class to generate random deals.
-count       : stop generation after this many successes
-accept      : a function which takes a Deal and returns True or False
-fixed_cards : a map direction -> hand string
+count       : stop generation after this many successes [None = infinite]
 rng         : an instance of random.Random()
 fail_count  : stop generation after this many failures in the accept clause
-    """
+    [None = infinite]
+west, north, east, south: can each be one of:
+    None - no restrictions on the hand
+    str - parsed as a Hand
+    Hand, PartialHand - use the 13 cards from the object
+    list, set - assumes 13 Card objects, sets exact value
+    callable - a function which takes a Hand and returns True/False\
+"""
 
     misses = 0
     hits = 0
+    dealer = RestrictedDealer(west, north, east, south, rng)
     while (count is None or hits<count) and (fail_count is None or misses<fail_count):
-        d = one_random_deal(fixed_cards, rng)
-        if accept is None or accept(d):
+        d = dealer.one_try()
+        if d is None:
+            misses += 1
+        else:
             hits += 1
             yield d
-        else:
-            misses += 1
 
 __all__ = ["random_deals"]
