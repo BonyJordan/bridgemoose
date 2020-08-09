955c85d579034b9fc261812a8d0d77365544fd8b
Jordan Lampe <jordan@jyjy.org>
2020-08-08 20:54:17 -0400

    Add bucketer

--- src/bridgemoose/stats.py
+++ src/bridgemoose/stats.py
@@ -1,5 +1,6 @@
 import math
 from collections import Counter
+from collections import defaultdict
 
 from . import dds
 from . import scoring
@@ -33,19 +34,22 @@ class Statistic:
         return "%.2f +/- %.2f" % (self.mean, self.std_error())
         
 def dd_compare_strategies(deal_generator, strategy1, strategy2,
-    score_type="IMPS", vulnerability=None):
+    score_type="IMPS", vulnerability=None, bucketer=None):
     """\
 Returns a triple; a Statistic and two collections.Counter objects.
 The Statistic returns the score from the NS point of view of strategy1
 vs. strategy2.  The Counters are essentially dicts mapping ("<contract-declarer>", score) pairs to counts.
 
-A "strategy" is a function which takes a Deal and returns a ContractDeclarer,
-which is a string like "3NT-W"
+A "strategy" can be either a contract-declarer string ("3NT-W") or a
+function which takes a Deal and returns a contract-declarer.
 
 "Scoring" can be 'IMPS', 'matchpoints', 'TOTAL'.
 
-"vulnerability" can be None, "", "NS", "EW", "BOTH", "NSEW"\
+"vulnerability" can be None, "", "NS", "EW", "BOTH", "NSEW"
 
+If bucketer is not None, it is a function which takes a Deal and returns
+a dict key, and then dd_compare_strategies returns a dict whose values
+are triples as described above.
     """
     if vulnerability is None:
         vul = ""
@@ -67,17 +71,13 @@ which is a string like "3NT-W"
 
     sign = {"N":1, "S":1, "E":-1, "W":-1}
 
-    stat = Statistic()
-    count1 = Counter()
-    count2 = Counter()
+    out = defaultdict(lambda: (Statistic(), Counter(), Counter()))
 
     for deal in deal_generator:
-        cd1 = strategy1(deal)
-        cd2 = strategy2(deal)
+        cd1 = strategy1(deal) if callable(strategy1) else strategy1
+        cd2 = strategy2(deal) if callable(strategy2) else strategy2
         con1, dec1 = cd1.split("-")
         con2, dec2 = cd2.split("-")
-        if (con1,dec1) == (con2,dec2):
-            stat.add_data_point(0.0)
 
         strain1 = con1[1]
         strain2 = con2[1]
@@ -95,9 +95,14 @@ which is a string like "3NT-W"
         score2 = sign[dec2] * scoring.result_score(con2,
             tx2, dec2 in vul)
 
+        b = "" if bucketer is None else bucketer(deal)
+        stat, count1, count2 = out[b]
+
         stat.add_data_point(score_func(score1 - score2))
         count1[(cd1, score1)] += 1
         count2[(cd2, score2)] += 1
 
-
-    return stat, count1, count2
+    if bucketer is None:
+        return out[""]
+    else:
+        return out
