338233665233910a09bd12d0d7def8a67b937ebd
Jordan Lampe <jordan@jyjy.org>
2020-08-11 22:45:38 -0400

    okay bring accepts back

--- src/bridgemoose/random.py
+++ src/bridgemoose/random.py
@@ -19,7 +19,7 @@ def parse_card_set(s):
     return out
 
 class RestrictedDealer:
-    def __init__(self, west=None, north=None, east=None, south=None, rng=None):
+    def __init__(self, west=None, north=None, east=None, south=None, deal_accept=None, rng=None):
         if rng is None:
             self.rng = random
             self.sorting = False
@@ -29,6 +29,7 @@ class RestrictedDealer:
         self.cardset = set(Card.all())
         self.acceptors = dict()
         self.known_cards = {d: set() for d in Direction.ALL}
+        self.deal_accept = deal_accept
 
         for d, spec in zip("WNES", [west, north, east, south]):
             if spec is None:
@@ -64,7 +65,12 @@ class RestrictedDealer:
         for d, acc in self.acceptors.items():
             if not acc(hands[d]):
                 return None
-        return Deal(hands['W'],hands['N'],hands['E'],hands['S'])
+
+        deal = Deal(hands['W'],hands['N'],hands['E'],hands['S'])
+        if self.deal_accept is not None:
+            if not self.deal_accept(deal):
+                return None
+        return deal
 
     def _process_card_set(self, d, cset):
         if len(cset) != 13:
@@ -78,7 +84,7 @@ class RestrictedDealer:
         self.known_cards[d] = cset
 
 def random_deals(count, west=None, north=None, east=None, south=None,
-    rng=None, fail_count=100000):
+    deal_accept=None, rng=None, fail_count=100000):
     """\
 Class to generate random deals.
 count       : stop generation after this many successes [None = infinite]
@@ -91,11 +97,12 @@ west, north, east, south: can each be one of:
     Hand, PartialHand - use the 13 cards from the object
     list, set - assumes 13 Card objects, sets exact value
     callable - a function which takes a Hand and returns True/False\
+deal_accept  : can be None or a callable which takes a Deal and returns True/False\
 """
 
     misses = 0
     hits = 0
-    dealer = RestrictedDealer(west, north, east, south, rng)
+    dealer = RestrictedDealer(west, north, east, south, deal_accept, rng)
     while (count is None or hits<count) and (fail_count is None or misses<fail_count):
         d = dealer.one_try()
         if d is None:
