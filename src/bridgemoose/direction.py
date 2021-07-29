70560fafbf9c6ca08431a36e79b6db5fe40b8692
Jordan Lampe <jordan@jyjy.org>
2021-07-29 09:30:50 -0400

    Added Vuln

--- src/bridgemoose/direction.py
+++ src/bridgemoose/direction.py
@@ -62,4 +62,54 @@ Direction.WEST = Direction("W")
 Direction.NORTH = Direction("N")
 Direction.SOUTH = Direction("S")
 
-__all__ = ["Direction"]
+class Vuln:
+    EW_BIT = 2
+    NS_BIT = 1
+
+    def __init__(self, data):
+        if isinstance(data, int):
+            if data < 0 or data > 3:
+                raise ValueError("Ints from 0 to 3 only")
+            else:
+                self.data = data
+        elif isinstance(data, str):
+            m = {"-": 0,
+                 "o": 0,
+                 "none": 0,
+                 "e": Vuln.EW_BIT,
+                 "ew": Vuln.EW_BIT,
+                 "n": Vuln.NS_BIT,
+                 "ns": Vuln.NS_BIT,
+                 "b": Vuln.EW_BIT | Vuln.NS_BIT,
+                 "both": Vuln.EW_BIT | Vuln.NS_BIT}
+            if data in m:
+                self.data = m[data]
+            else:
+                raise ValueError("Strings are from -,e,n,b")
+        elif isinstance(data, Vuln):
+            self.data = data.data
+        else:
+            raise TypeError("Unexpected type")
+
+    def ew_vul(self):
+        return (self.data & Vuln.EW_BIT) != 0
+    def ns_vul(self):
+        return (self.data & Vuln.NS_BIT) != 0
+        return (self.data & Vuln.EW_BIT) != 0
+
+    def __str__(self):
+        return "-neb"[self.data]
+    def __repr__(self):
+        return "Vuln(%s)" % (self.__str__())
+
+    def __eq__(self, other):
+        return isinstance(other, Vuln) and self.data == other.data
+    def __hash__(self):
+        return hash(self.data)
+
+    @staticmethod
+    def all_vulns():
+        return list([Vuln(i) for i in range(4)])
+
+
+__all__ = ["Direction", "Vuln"]
