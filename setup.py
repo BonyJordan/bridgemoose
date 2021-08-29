dd7dceaf7b8a19b41faf53f4d509e11320a9250e
Jordan Lampe <jordan@jyjy.org>
2021-08-29 18:37:02 -0400

    Since we're not using threads anyway, don't need to compile 'em in

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 module_dds = setuptools.Extension('bridgemoose.dds',
     # define_macros = [('DDS_THREADS_GCD', None), ('DDS_THREADS_STL', None)],
-    define_macros = [('DDS_THREADS_STL', None)],
+    # define_macros = [('DDS_THREADS_STL', None)],
     extra_compile_args = ['-std=c++11'],
     sources=[os.path.join('dds', x) for x in [
         "ABsearch.cpp",
